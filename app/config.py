c63dfe0d7ced7bde14d0faf260bd61b7cc621e05
ivan <new.people089@gmail.com>
2026-06-22 07:53:27 +0300

    добавлен конфигурационный файл проекта

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+class Confing:
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
+    MAX_CONTENT_LENGHT = 16 * 1024 * 1024
