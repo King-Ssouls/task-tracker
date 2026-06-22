ff1c99f777e793710fc280fa9d02ea6a8c8daf20
ivan <new.people089@gmail.com>
2026-06-23 01:40:55 +0300

    переписание __init__ файла

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,26 @@
 import os
 from flask import Flask
-from .config import Confing
+from .config import Config
 from .extensions import db, migrate, login_manager
 
-def create_app():
+
+def create_app(test_config=None):
     app = Flask(__name__)
-    
-    app.config.from_object(Confing)
+
+    app.config.from_object(Config)
+
+    if test_config:
+        app.config.update(test_config)
+
     os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
-    
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
     
     # подключение flask логинов
     login_manager.init_app(app)
     login_manager.login_view = "main.login"
-    
+
     from .routes import main
+    app.register_blueprint(main)
     return app
